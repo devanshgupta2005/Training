81c96a595463c06675cddfa4afcd242a87964a5b
devanshgupta2005 <148710067+devanshgupta2005@users.noreply.github.com>
2026-01-13 12:27:52 +0530

    new code added

--- 13_1.py
+++ 13_1.py
@@ -38,5 +38,49 @@ if password == correct_password:
 else:
     print("Access Denied")
 
+# This program checks if a number is positive, negative, or zero.   
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("Number is positive")
+elif n < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
+
+
+# This program assigns a grade based on the marks entered by the user.
+marks = int(input("Enter marks: "))
+
+if marks >= 90 and marks <= 100:
+    print("Grade A")
+elif marks >= 75 and marks < 90:
+    print("Grade B")
+elif marks >= 50 and marks < 75 :
+    print("Grade C")
+elif marks >100:
+    print("Invalid marks")
+else:
+    print("Fail")
+
+
+# # Program to check password
+
+# Predefined correct password
+correct_password = "python123"
+
+# Taking input from the user
+password = input("Enter password: ")
+
+# Checking password
+if password == correct_password:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+
+
+
+
 
 
