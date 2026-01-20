857b58ce68d37304c99747ebda5539f1c7df0bc0
devanshgupta2005 <148710067+devanshgupta2005@users.noreply.github.com>
2026-01-20 12:49:05 +0530

    Create 20_1.py
    file handling

---
+++ 20_1.py
@@ -0,0 +1,24 @@
+# ##
+# with open(r"C:\Users\devan\OneDrive\Desktop\20_1.txt" , "r") as file:
+#     content = file.read( )
+#     print ( content ) 
+
+# ##
+# with open(r"C:\Users\devan\OneDrive\Desktop\20_1.txt" , "r") as file:
+#     content = file.readlines( )
+#     print ( content ) 
+
+# ##
+# with open(r"C:\Users\devan\OneDrive\Desktop\20_1.txt" , "r") as file:
+#     for line in file:
+#         print(line) 
+
+##
+with open(r"C:\Users\devan\OneDrive\Desktop\20_1.txt" , "a") as file:
+    file.write("Hello, World!\n")
+    file.write("\nWelcome to file handling in Python.\n")
+    file.write("\nThis is a new line added to the file.\n")
+    print(file)
+
+    ##
+    
